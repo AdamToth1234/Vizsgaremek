cb00497f8a148e65c789c9d776b4ef9f617f8ea3
Tóth Ádám <tothadam1010@gmail.com>
2024-11-01 22:01:16 +0100

    Finished designing the hamburger overlay slide and some minor bugfixes

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,7 @@ const closeButton = document.querySelector(".close")
 const hamburger = document.querySelector(".hamburger")
 const overlay = document.querySelector(".overlay")
 const menuOverlay = Array.from(document.querySelector(".menu-overlay").children)
+const body = document.body;
 
 
 menuOverlay.forEach(element => {
@@ -14,9 +15,11 @@ menuOverlay.forEach(element => {
 
 hamburger.addEventListener("click", () => {
     overlay.classList.add("transform")
+    body.classList.toggle('menu-open');
 })
 
 
 closeButton.addEventListener("click", () => {
     overlay.classList.remove("transform")
+    body.classList.toggle('menu-open');
 })
