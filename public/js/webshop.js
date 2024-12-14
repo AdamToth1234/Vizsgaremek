76a31555097e3fc645d080980907b0c4b3482f64
Tóth Ádám <tothadam1010@gmail.com>
2024-12-14 15:39:29 +0100

    Some minor changes on the webshop site

--- public/js/webshop.js
+++ public/js/webshop.js
@@ -1,9 +1,31 @@
-const kaka = document.querySelector(".book")
+const arrowRight = document.querySelectorAll(".fa-chevron-right")
+let counter = 0
 
-fetch("/konyvek")
-.then(result => result.json())
-.then(data => console.log(data))
+const translateObject = {
+    0: "-1350px",
+    1: "-2700px",
+    2: "-3050px",
+    3: "-4400px"
+}
 
-kaka.addEventListener("click", (e) => {
-    console.log(e.currentTarget.getAttribute("data-id"));
+arrowRight.forEach(i => {
+    i.addEventListener("click", (e) => {
+        const currentBooks = Array.from(e.currentTarget.parentElement.parentElement.parentElement.parentElement.childNodes[3].childNodes).filter(node => node.nodeType === 1)
+        console.log();
+        
+        currentBooks.forEach(f => {
+            f.style.transform = `translate3d(${translateObject[counter]}, 0, 0)`
+        })
+
+        const currentArrowsParent = Array.from(e.currentTarget.parentElement.parentElement.childNodes).filter(node => node.nodeType === 1)
+        currentArrowsParent.forEach(f => {
+            f.classList.add("active")
+        });
+    })
 })
+
+
+
+// fetch("/konyvek")
+// .then(result => result.json())
+// .then(data => console.log(data))
